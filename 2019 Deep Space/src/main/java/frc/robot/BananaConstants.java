aad6796390dc1d55be954ca2e21ad3102808ddda
Alex Naehu <117035077+AlexNaehu@users.noreply.github.com>
2023-01-13 19:35:37 -0800

    Update and rename CatzConstants.java to BananaConstants.java

---
+++ 2019 Deep Space/src/main/java/frc/robot/BananaConstants.java
@@ -0,0 +1,15 @@
+package frc.robot;
+/*
+ *  Author : Alex Naehu
+
+ *  Methods : 
+ *  Functionality : Holds all global data values
+ *   
+ *  Revision History : 
+ *  First Created 1/13/23
+ * 
+ */
+public class CatzConstants
+{
+   
+}
