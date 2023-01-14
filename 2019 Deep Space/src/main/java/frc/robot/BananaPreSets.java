948ee456a406f3886682e634db6c7f62948d47d3
Alex Naehu <117035077+AlexNaehu@users.noreply.github.com>
2023-01-13 19:40:55 -0800

    Update BananaPreSets.java

--- 2019 Deep Space/src/main/java/frc/robot/BananaPreSets.java
+++ 2019 Deep Space/src/main/java/frc/robot/BananaPreSets.java
@@ -1,5 +1,16 @@
 package frc.robot;
 
+/*
+ *  Author : Alex Naehu
+ *  Methods : 
+ *  Functionality : Holds all presetables, i.e. game piece dimensions or specific arm positions, etc.
+ *   
+ *  Revision History : 
+ *  First Created 1/13/23
+ * 
+ */
+
+//idea: do 3 different levels of arm positions for 3 different scoring types (3 different heights to raise the arm)
 
 public class BananaPreSets
 {
