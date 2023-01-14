ddc30f4ab31f54f30bc6a74a4b0197d8706eb065
Alex Naehu <117035077+AlexNaehu@users.noreply.github.com>
2023-01-13 19:54:41 -0800

    Update and rename CatzAutonomousPaths.java to BananaAutonomousPaths.java

--- 2019 Deep Space/src/main/java/frc/Autonomous/BananaAutonomousPaths.java
+++ 2019 Deep Space/src/main/java/frc/Autonomous/BananaAutonomousPaths.java
@@ -1,6 +1,6 @@
 package frc.Autonomous;
 
-public class CatzAutonomousPaths 
+public class BananaAutonomousPaths 
 {
           final double turn_R_90 = 90.0;
           final double turn_L_90 = -90.0;
