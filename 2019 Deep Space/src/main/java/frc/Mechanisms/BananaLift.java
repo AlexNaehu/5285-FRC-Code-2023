91d1e491f15439a54643be52d000f7b451a588fb
Alex Naehu <117035077+AlexNaehu@users.noreply.github.com>
2023-01-13 19:26:04 -0800

    Update and rename CatzLift.java to BananaLift.java

---
+++ 2019 Deep Space/src/main/java/frc/Mechanisms/BananaLift.java
@@ -0,0 +1,21 @@
+
+/*
+ *  Author : Alex Naehu
+ * 
+ *  Functionality : 
+ *   
+ *  Methods :  
+ *
+ *  Revision History : First Created 1/13/23
+ * 
+ * 
+ */
+
+package frc.Mechanisms;
+
+    public BananaLift()
+    {
+        
+    
+}
+
