384b335c1839afd5fa9631e7ba9a3f549be38d52
Alex Naehu <117035077+AlexNaehu@users.noreply.github.com>
2023-01-15 12:39:31 -0800

    Update BananaPreSets.java

--- 2019 Deep Space/src/main/java/frc/robot/BananaPreSets.java
+++ 2019 Deep Space/src/main/java/frc/robot/BananaPreSets.java
@@ -11,6 +11,7 @@ package frc.robot;
  */
 
 //idea: do 3 different levels of arm positions for 3 different scoring types (3 different heights to raise the arm)
+//idea: do one angle to pick up rigth side up cones and cubes, do another angle to pickup at the single drop station
 
 
 
@@ -19,7 +20,7 @@ import edu.wpi.first.wpilibj.Timer;
 public class CatzPreSets
 {
     //Values for ball and hatch pickup
-    private static double NEUTRAL_PIVOT_ANGLE = 0.0;
+    private static double NEUTRAL_PIVOT_ANGLE = 0.0;// 0.0 angle is at straight verticle down
     
  
 
@@ -27,19 +28,7 @@ public class CatzPreSets
     
 
 
-    /**
-     * 
-     *  Values for stowing arm
-     * 
-     */
-    private static double PRESTOWED_PIVOT_ANGLE = 50.0;
-   
-
-    private static double PRESTOWED2_PIVOT_ANGLE = 55.0;
-    private static double PRESTOWED2_WRIST_ANGLE = 40;// ??
     
-    private static double STOWED_PIVOT_ANGLE = 29;
-    private static double STOWED_WRIST_ANGLE = 78;
 
     /**
      * 
@@ -47,19 +36,14 @@ public class CatzPreSets
      * 
      */
     private static double TRAVEL_PIVOT_ANGLE = 80.0;
-    private static double TRAVEL_ARM_EXT     = 1.0;
-    private static double TRAVEL_WRIST_ANGLE = 47.0;
+    
 
     //Values for scoring in the cargo bay
     private static double CARGO_BAY_BALL_PIVOT_ANGLE = 110.0;
-    private static double CARGO_BAY_BALL_ARM_EXT     = 9.5;
-    private static double CARGO_BAY_BALL_WRIST_ANGLE = 55.6;
-    private static double CARGO_BAY_BALL_LIFT_HEIGHT = 0.0;
+   
 
     private static double CARGO_BAY_HATCH_PIVOT_ANGLE = 21.0;
-    private static double CARGO_BAY_HATCH_ARM_EXT     = 3.0;
-    private static double CARGO_BAY_HATCH_WRIST_ANGLE = -100.0;
-    private static double CARGO_BAY_HATCH_LIFT_HEIGHT = 0.0;
+   
 
     
     /**
@@ -68,19 +52,13 @@ public class CatzPreSets
      * 
      */
     private static double LVL_3_BALL_PIVOT_ANGLE = 90;
-    private static double LVL_3_BALL_ARM_EXT = 15;
-    private static double LVL_3_BALL_WRIST_ANGLE = 15; 
-    private static double LVL_3_BALL_LIFT_HEIGHT = 0.0; //TBD
+    
 
     private static double LVL_2_BALL_PIVOT_ANGLE = 101.4;
-    private static double LVL_2_BALL_ARM_EXT = 16.4;
-    private static double LVL_2_BALL_WRIST_ANGLE = 29.5;
-    private static double LVL_2_BALL_LIFT_HEIGHT = 0.0;
+   
 
     private static double LVL_1_BALL_PIVOT_ANGLE = 53.0;
-    private static double LVL_1_BALL_ARM_EXT     = 0.0;
-    private static double LVL_1_BALL_WRIST_ANGLE = -5.0;
-    private static double LVL_1_BALL_LIFT_HEIGHT = 0.0;
+
 
 
     /**
@@ -89,38 +67,27 @@ public class CatzPreSets
      * 
      */
     private static double LVL_3_HATCH_PIVOT_ANGLE = 104.0;
-    private static double LVL_3_HATCH_ARM_EXT     = 15;
-    private static double LVL_3_HATCH_WRIST_ANGLE = -38.6; 
-    private static double LVL_3_HATCH_LIFT_HEIGHT = 0.0; //TBD
+   
 
     private static double LVL_2_HATCH_PIVOT_ANGLE = 100.0;
-    private static double LVL_2_HATCH_ARM_EXT = 13.1;
-    private static double LVL_2_HATCH_WRIST_ANGLE = -42.4;
-    private static double LVL_2_HATCH_LIFT_HEIGHT = 0.0;
+  
 
     private static double LVL_1_HATCH_PIVOT_ANGLE = 21.0;
-    private static double LVL_1_HATCH_ARM_EXT = 3.0;
-    private static double LVL_1_HATCH_WRIST_ANGLE = -100.0;
-    private static double LVL_1_HATCH_LIFT_HEIGHT = 0.0;
+
 
 
     /** 
      *  Values for scoring in the cargo bay on the back side of the robot
     */
-    private static double CARGO_BAY_REV_BALL_PIVOT_ANGLE = 155.0;
-    private static double CARGO_BAY_REV_BALL_WRIST_ANGLE = -48.6;
-    private static double CARGO_BAY_REV_BALL_ARM_EXT     = 9.844;
-
-    public static void setPosition(double pivotTargetAngle, double armTargetExtension, 
-                                   double wristTargetAngle, double liftTargetHeight)
+    private static double CARGO_BAY_REV_BALL_PIVOT_ANGLE = 155.0;//change ball to cube, and add a scoring angle 
+                                                                //might be different if you have to lift the arm higher for the peg
+    
+    public static void setPosition(double pivotTargetAngle)
     {
         Robot.arm.setPivotTargetAngle(pivotTargetAngle);
 
         Robot.arm.setArmTargetHit(false);
-        Robot.arm.setArmTargetExt(armTargetExtension);
-        
-        Robot.intake.setWristTargetAngle(wristTargetAngle);
-        Robot.lift.setLiftTargetHeight(liftTargetHeight);
+       
     }
 
     public static void cargoPickUp()
@@ -128,15 +95,13 @@ public class CatzPreSets
         /*Robot.intake.setWristTargetAngle(NEUTRAL_WRIST_ANGLE);
         Timer.delay(0.2); //Wait for wrist to be positioned not to hit the ground */
 
-        Robot.intake.setWristTargetAngle(NEUTRAL_WRIST_ANGLE); //testing
-        Timer.delay(0.2);
+ 
 
-        Robot.arm.setArmTargetHit(false);
-        Robot.arm.setArmTargetExt(NEUTRAL_ARM_EXT);
+        
         Timer.delay(0.3);
 
         Robot.arm.setPivotTargetAngle(NEUTRAL_PIVOT_ANGLE);
-        
+        Robot.arm.setArmTargetHit(false);
         //Timer.delay(0.2);
 
         //Timer.delay(0.3);
@@ -146,14 +111,11 @@ public class CatzPreSets
         
     }
 
-    public static void hatchPickUp()
+    public static void hatchPickUp() 
     {
-        Robot.arm.setPivotTargetAngle(HATCH_PICKUP_PIVOT_ANGLE);
+        Robot.arm.setPivotTargetAngle(HATCH_PICKUP_PIVOT_ANGLE); //incase we need a different angle for manually adding game pieces to the field via hatch
         Robot.arm.setArmTargetHit(false);
-        Robot.arm.setArmTargetExt(HATCH_PICKUP_ARM_EXT);
      
-        Robot.intake.setWristTargetAngle(HATCH_PICKUP_WRIST_ANGLE);
-        Robot.lift.setLiftTargetHeight(HATCH_PICKUP_LIFT_HEIGHT);
     }
 
 
@@ -171,7 +133,7 @@ public class CatzPreSets
         Robot.arm.setPivotTargetAngle(CARGO_BAY_BALL_PIVOT_ANGLE);
         Timer.delay(0.0100);
 
-        Robot.intake.setWristTargetAngle(CARGO_BAY_BALL_WRIST_ANGLE);
+        
     }
 
     public static void cargoBayHatch()
@@ -198,10 +160,7 @@ public class CatzPreSets
         Robot.arm.setPivotTargetAngle(LVL_3_BALL_PIVOT_ANGLE);
 
         Robot.arm.setArmTargetHit(false);
-        Robot.arm.setArmTargetExt(LVL_3_BALL_ARM_EXT);
-
-        Robot.intake.setWristTargetAngle(LVL_3_BALL_WRIST_ANGLE);
-        Robot.lift.setLiftTargetHeight(LVL_3_BALL_LIFT_HEIGHT);
+       
     }
 
     public static void lvl2RocketBall()
@@ -209,10 +168,7 @@ public class CatzPreSets
         Robot.arm.setPivotTargetAngle(LVL_2_BALL_PIVOT_ANGLE);
 
         Robot.arm.setArmTargetHit(false);
-        Robot.arm.setArmTargetExt(LVL_2_BALL_ARM_EXT);
-
-        Robot.intake.setWristTargetAngle(LVL_2_BALL_WRIST_ANGLE);
-        Robot.lift.setLiftTargetHeight(LVL_2_BALL_LIFT_HEIGHT);
+        
     }
 
     public static void lvl1RocketBall()
@@ -221,10 +177,7 @@ public class CatzPreSets
         Timer.delay(0.2);
 
         Robot.arm.setArmTargetHit(false);
-        Robot.arm.setArmTargetExt(LVL_1_BALL_ARM_EXT);
-
-        Robot.intake.setWristTargetAngle(LVL_1_BALL_WRIST_ANGLE);
-        Robot.lift.setLiftTargetHeight(LVL_1_BALL_LIFT_HEIGHT);
+       
     }
 
 
