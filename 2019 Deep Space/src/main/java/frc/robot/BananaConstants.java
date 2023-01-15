2461455df978b1f6620379f9628cf534c7d3a3e9
Alex Naehu <117035077+AlexNaehu@users.noreply.github.com>
2023-01-15 15:35:44 -0800

    Update BananaConstants.java

--- 2019 Deep Space/src/main/java/frc/robot/BananaConstants.java
+++ 2019 Deep Space/src/main/java/frc/robot/BananaConstants.java
@@ -11,5 +11,16 @@ package frc.robot;
  */
 public class CatzConstants
 {
+    public static final double ROBOT_LENGTH = 0; //TBD
+    public static final double ROBOT_WIDTH = 0; //TBD
+    public static final double HALF_ROBOT_LENGTH = ROBOT_LENGTH/2;//depends on where we put the pivot point for the arm
+    public static final double HALF_ROBOT_WIDTH = ROBOT_WIDTH/2;
+
+    public static boolean USING_COMPETITION_ROBOT = true;
+
+    public static final double CONTROLLER_INPUT_WAIT_TIME = 0.005;
+
+    public static final double INVALID_ANGLE = 999.9;
    
+    //add more global variables if nessessary
 }
